f9500f8d4748c70c40b7982c0f7e5fb778fc8cbc
Gilson da Gama <gilsondagama@hotmail.com>
2020-06-06 06:06:01 -0300

    Controllers, Routes, Backend completed.

--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,9 +1,18 @@
-import express from "express"
+import express from "express";
+
+import PointsController from "./controllers/PointsController";
+import ItemsController from "./controllers/ItemsController";
+
+const pointsController = new PointsController();
+const itemsController = new ItemsController();
 
 const routes = express.Router();
 
-routes.get("/", (request,response) => {
-  return response.json({message: 'Hello world'})
-})
+routes.get("/items", itemsController.index);
 
-export default routes
+routes.post("/points", pointsController.create);
+routes.get("/points", pointsController.index);
+routes.get("/points/:id", pointsController.show);
+routes.delete("/points/:id", pointsController.delete);
+
+export default routes;
